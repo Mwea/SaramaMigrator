44aebb9d4e71ce726556fe52dba183b7b5c0feb5
Titouan Chary <titouan.chary@datadoghq.com>
2020-10-13 10:47:52 +0200

    Add dependency to testify and make tests timeout if run for too long

--- consumer_test.go
+++ consumer_test.go
@@ -4,9 +4,12 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/Shopify/sarama"
+	"github.com/stretchr/testify/assert"
 )
 
 var testMsg = StringEncoder("Foo")
@@ -18,6 +21,8 @@ func init() {
 // If a particular offset is provided then messages are consumed starting from
 // that offset.
 func TestConsumerOffsetManual(t *testing.T) {
+	go FailOnTimeout(t, 5*time.Second)
+
 	// Given
 	broker0 := sarama.NewMockBroker(t, 0)
 
@@ -68,6 +73,8 @@ func TestConsumerOffsetManual(t *testing.T) {
 // message is indeed corresponds to the offset that broker claims to be the
 // newest in its metadata response.
 func TestConsumersaramaOffsetNewest(t *testing.T) {
+	go FailOnTimeout(t, 5*time.Second)
+
 	// Given
 	broker0 := sarama.NewMockBroker(t, 0)
 	broker0.SetHandlerByMap(map[string]sarama.MockResponse{
@@ -98,9 +105,8 @@ func TestConsumersaramaOffsetNewest(t *testing.T) {
 
 	// Then
 	assertMessageOffset(t, <-consumer.Messages(), 10)
-	if hwmo := consumer.HighWaterMarkOffset(); hwmo != 14 {
-		t.Errorf("Expected high water mark offset 14, found %d", hwmo)
-	}
+	hwo := consumer.HighWaterMarkOffset()
+	assert.EqualValues(t, hwo, 14, "Expected high water mark offset 14, found %d", hwo)
 
 	safeClose(t, consumer)
 	safeClose(t, master)
@@ -109,6 +115,8 @@ func TestConsumersaramaOffsetNewest(t *testing.T) {
 
 // It is possible to close a partition consumer and create the same anew.
 func TestConsumerRecreate(t *testing.T) {
+	go FailOnTimeout(t, 5*time.Second)
+
 	// Given
 	broker0 := sarama.NewMockBroker(t, 0)
 	broker0.SetHandlerByMap(map[string]sarama.MockResponse{
@@ -151,6 +159,8 @@ func TestConsumerRecreate(t *testing.T) {
 
 // An attempt to consume the same partition twice should fail.
 func TestConsumerDuplicate(t *testing.T) {
+	go FailOnTimeout(t, 5*time.Second)
+
 	// Given
 	broker0 := sarama.NewMockBroker(t, 0)
 	broker0.SetHandlerByMap(map[string]sarama.MockResponse{
@@ -160,7 +170,7 @@ func TestConsumerDuplicate(t *testing.T) {
 		"OffsetRequest": sarama.NewMockOffsetResponse(t).
 			SetOffset("my_topic", 0, sarama.OffsetOldest, 0).
 			SetOffset("my_topic", 0, sarama.OffsetNewest, 1000),
-		"FetchRequest":       sarama.NewMockFetchResponse(t, 1),
+		"FetchRequest":       sarama.NewMockFetchResponse(t, 1).SetVersion(4),
 		"ApiVersionsRequest": sarama.NewMockApiVersionsResponse(t),
 	})
 
@@ -178,19 +188,142 @@ func TestConsumerDuplicate(t *testing.T) {
 	pc2, err := c.ConsumePartition("my_topic", 0, 0)
 
 	// Then
-	if pc2 != nil || err != sarama.ConfigurationError("That topic/partition is already being consumed") {
-		t.Fatal("A partition cannot be consumed twice at the same time")
-	}
+	assert.Equal(t, pc2, nil)
+	assert.Equal(t, err, sarama.ConfigurationError("That topic/partition is already being consumed"))
 
 	safeClose(t, pc1)
 	safeClose(t, c)
 	broker0.Close()
 }
 
-func assertMessageOffset(t *testing.T, msg *sarama.ConsumerMessage, expectedOffset int64) {
-	if msg.Offset != expectedOffset {
-		t.Errorf("Incorrect message offset: expected=%d, actual=%d", expectedOffset, msg.Offset)
+// StdLogger is used to log error messages.
+type StdLogger interface {
+	Print(v ...interface{})
+	Printf(format string, v ...interface{})
+	Println(v ...interface{})
+}
+
+func runConsumerLeaderRefreshErrorTestWithConfig(t *testing.T, config *sarama.Config) {
+	var Logger StdLogger = log.New(os.Stdout, "[Sarama] ", log.LstdFlags)
+	// Given
+	broker0 := sarama.NewMockBroker(t, 100)
+
+	// Stage 1: my_topic/0 served by broker0
+	Logger.Printf("    STAGE 1")
+
+	broker0.SetHandlerByMap(map[string]sarama.MockResponse{
+		"MetadataRequest": sarama.NewMockMetadataResponse(t).
+			SetBroker(broker0.Addr(), broker0.BrokerID()).
+			SetLeader("my_topic", 0, broker0.BrokerID()),
+		"OffsetRequest": sarama.NewMockOffsetResponse(t).
+			SetOffset("my_topic", 0, sarama.OffsetOldest, 123).
+			SetOffset("my_topic", 0, sarama.OffsetNewest, 1000),
+		"FetchRequest": sarama.NewMockFetchResponse(t, 1).
+			SetMessage("my_topic", 0, 123, testMsg).SetVersion(4),
+		"ApiVersionsRequest": sarama.NewMockApiVersionsResponse(t),
+	})
+
+	c, err := NewTransitioningConsumer([]string{broker0.Addr()}, config)
+	if err != nil {
+		t.Fatal(err)
 	}
+
+	pc, err := c.ConsumePartition("my_topic", 0, sarama.OffsetOldest)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assertMessageOffset(t, <-pc.Messages(), 123)
+
+	// Stage 2: broker0 says that it is no longer the leader for my_topic/0,
+	// but the requests to retrieve metadata fail with network timeout.
+	Logger.Printf("    STAGE 2")
+
+	fetchResponse2 := sarama.NewMockFetchResponse(t, 1)
+	fetchResponse2.SetError("my_topic", 0, sarama.ErrNotLeaderForPartition).SetVersion(4)
+
+	broker0.SetHandlerByMap(map[string]sarama.MockResponse{
+		"FetchRequest":       fetchResponse2,
+		"ApiVersionsRequest": sarama.NewMockApiVersionsResponse(t),
+	})
+
+	if consErr := <-pc.Errors(); consErr.Err != sarama.ErrOutOfBrokers {
+		t.Errorf("Unexpected error: %v", consErr.Err)
+	}
+
+	// Stage 3: finally the metadata returned by broker0 tells that broker1 is
+	// a new leader for my_topic/0. Consumption resumes.
+
+	Logger.Printf("    STAGE 3")
+
+	broker1 := sarama.NewMockBroker(t, 101)
+
+	broker1.SetHandlerByMap(map[string]sarama.MockResponse{
+		"FetchRequest": sarama.NewMockFetchResponse(t, 1).
+			SetMessage("my_topic", 0, 124, testMsg).SetVersion(4),
+		"ApiVersionsRequest": sarama.NewMockApiVersionsResponse(t),
+	})
+	broker0.SetHandlerByMap(map[string]sarama.MockResponse{
+		"MetadataRequest": sarama.NewMockMetadataResponse(t).
+			SetBroker(broker0.Addr(), broker0.BrokerID()).
+			SetBroker(broker1.Addr(), broker1.BrokerID()).
+			SetLeader("my_topic", 0, broker1.BrokerID()),
+		"ApiVersionsRequest": sarama.NewMockApiVersionsResponse(t),
+	})
+
+	assertMessageOffset(t, <-pc.Messages(), 124)
+
+	safeClose(t, pc)
+	safeClose(t, c)
+	broker1.Close()
+	broker0.Close()
+}
+
+// NewTestConfig returns a config meant to be used by tests.
+// Due to inconsistencies with the request versions the clients send using the default Kafka version
+// and the response versions our mocks use, we default to the minimum Kafka version in most tests
+func NewTestConfig() *sarama.Config {
+	config := sarama.NewConfig()
+	config.Version = sarama.MinVersion
+	return config
+}
+
+// If consumer fails to refresh metadata it keeps retrying with frequency
+// specified by `Config.Consumer.Retry.Backoff`.
+func TestConsumerLeaderRefreshError(t *testing.T) {
+	go FailOnTimeout(t, 5*time.Second)
+
+	config := NewTestConfig()
+	config.Net.ReadTimeout = 100 * time.Millisecond
+	config.Consumer.Retry.Backoff = 200 * time.Millisecond
+	config.Consumer.Return.Errors = true
+	config.Metadata.Retry.Max = 0
+	runConsumerLeaderRefreshErrorTestWithConfig(t, config)
+}
+
+func TestConsumerLeaderRefreshErrorWithBackoffFunc(t *testing.T) {
+	go FailOnTimeout(t, 5*time.Second)
+
+	var calls int32 = 0
+	config := NewTestConfig()
+	config.Net.ReadTimeout = 100 * time.Millisecond
+	config.Consumer.Retry.BackoffFunc = func(retries int) time.Duration {
+		atomic.AddInt32(&calls, 1)
+		return 200 * time.Millisecond
+	}
+	config.Consumer.Return.Errors = true
+	config.Metadata.Retry.Max = 0
+
+	runConsumerLeaderRefreshErrorTestWithConfig(t, config)
+
+	// we expect at least one call to our backoff function
+	if calls == 0 {
+		t.Fail()
+	}
+}
+
+func assertMessageOffset(t *testing.T, msg *sarama.ConsumerMessage, expectedOffset int64) {
+	assert.Equalf(t, msg.Offset, expectedOffset, "Incorrect message offset: expected=%d, actual=%d", expectedOffset, msg.Offset)
 }
 
 func safeClose(t testing.TB, c io.Closer) {
@@ -198,4 +331,10 @@ func safeClose(t testing.TB, c io.Closer) {
 	if err != nil {
 		t.Error(err)
 	}
+}
+
+func FailOnTimeout(t *testing.T, d time.Duration) {
+	<-time.After(d)
+	t.Errorf("Failed because of timeout")
+	panic("Failed because of timeout")
 }
